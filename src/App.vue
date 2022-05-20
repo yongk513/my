e2506994e55d85b9bc531ca0c3ad225864933bda
yongk <yongk513@163.com>
2022-05-20 19:27:02 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-// 随便改改
+// 随便改改s
 useHead({
   title: 'index',
   meta: [
