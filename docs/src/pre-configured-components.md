4ce8375041faa71c731a62a0a9c26a5c04316e0c
yongk <hi@yongk.me>
2023-04-25 16:08:28 +0800

    Initial commit

---
+++ docs/src/pre-configured-components.md
@@ -0,0 +1,13 @@
+# Pre-configured Components
+
+- [Vite](https://vitejs.dev)
+- [Electron](https://www.electronjs.org)
+- [Electron Builder](https://www.electron.build)
+- [TypeScript](https://www.typescriptlang.org)
+- [Vue](https://vuejs.org)
+- [Vue-i18n](https://kazupon.github.io/vue-i18n)
+- [Vue-router](https://router.vuejs.org)
+- [Vuetify](https://vuetifyjs.com)
+- [Pinia](https://pinia.vuejs.org)
+- [ESLint](https://eslint.org)
+- [Prettier](https://prettier.io)
---
+++ src/main/IPCs.ts
@@ -0,0 +1,19 @@
+import { BrowserWindow, ipcMain, shell } from 'electron'
+import Constants from './utils/Constants'
+
+/*
+ * IPC Communications
+ * */
+export default class IPCs {
+  static initialize(window: BrowserWindow): void {
+    // Get application version
+    ipcMain.on('msgRequestGetVersion', () => {
+      window.webContents.send('msgReceivedVersion', Constants.APP_VERSION)
+    })
+
+    // Open url via web browser
+    ipcMain.on('msgOpenExternalLink', async (event, url: string) => {
+      await shell.openExternal(url)
+    })
+  }
+}
